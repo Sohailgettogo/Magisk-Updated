b46e68fe92d496b702339175961df0c1551afc3c
Sohailgettogo <sohailshinestar@gmail.com>
2026-09-04 13:08:47 +0000

    Remove undeclared skip_initramfs line and bypass do_skip_initramfs

--- init/initramfs.c
+++ init/initramfs.c
@@ -615,7 +615,6 @@ static int __init skip_initramfs_param(char *str)
 {
 	if (*str)
 		return 0;
-	do_skip_initramfs = 0;
 	return 1;
 }
 __setup("skip_initramfs", skip_initramfs_param);
@@ -623,7 +622,6 @@ __setup("skip_initramfs", skip_initramfs_param);
 
 static int __init populate_rootfs(void)
 {
-	skip_initramfs = 0;
 	char *err;
 
 #ifdef CONFIG_INITRAMFS_SKIP
